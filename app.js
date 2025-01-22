99bd5c782e0442b6939e98302f364d36d8a6eb40
MiitchPs <miitch_ps@outlook.com>
2025-01-22 19:04:00 -0300

    Funcion para sortear los amigos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+let amigoSorteado = "";
 
 
 // Función para agregar un amigo a la lista
@@ -26,9 +27,27 @@ function mostrarLista() {
     //bucle for para recorrer el arreglo amigos
     for (let i = 0; i < amigos.length; i++) {
         const li = document.createElement('li'); // Crear un nuevo elemento <li>
-        li.textContent = amigos[i]; // Asignar el nombre al contenido del <li>
-        listaAmigos.appendChild(li); // Agregar el <li> a la lista
+        li.textContent = amigos[i]; // Asigna el nombre al contenido del <li>
+        listaAmigos.appendChild(li); // Agrega el <li> a la lista
     }
 }
+
+// Función para sortear un amigo
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear. Agrega nombres primero.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    amigoSorteado = amigos[indiceAleatorio];
+
+    // Actualiza el contenido del elemento de resultado utilizando document.getElementById() e innerHTML
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El Amigo secreto sorteado es: ${amigoSorteado}`;
+}
+
+
+
 
 
