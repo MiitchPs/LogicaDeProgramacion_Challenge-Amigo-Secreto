88ba083a0967aafd619c699890ac7feab793e877
MiitchPs <miitch_ps@outlook.com>
2025-01-22 18:07:24 -0300

    Array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let amigos = [];
 
-// Función para agregar un amigo
+
+// Función para agregar un amigo a la lista
 function agregarAmigo() {
     const input = document.getElementById('amigo');
     const nombre = input.value.trim();
